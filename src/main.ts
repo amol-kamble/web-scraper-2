2a7f81acef1d3bf7831e6052365d92626a5b10e8
amol-kamble <amol.pkl@gmail.com>
2021-12-09 15:47:42 +0530

    enable cors allow

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: false,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   console.log(`${join(__dirname, '..', 'resources')}`);
   app.useStaticAssets(join(__dirname, '..', 'resources'));
-
+  app.enableCors({
+    origin : '*',
+  })
   await app.listen(3000);
 }
 bootstrap();
